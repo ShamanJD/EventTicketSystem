b331e09fc26749e65c7fb09f11cba1d941842e75
ShamanJD <eugene.ptashko@gmail.com>
2025-12-28 01:45:27 +0500

    hotfix

--- src/EventTicket.Api/Controllers/BookingController.cs
+++ src/EventTicket.Api/Controllers/BookingController.cs
@@ -1,3 +1,4 @@
+using EventTicket.Application.Services;
 using EventTicket.Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Mvc;
@@ -6,18 +7,18 @@ namespace EventTicket.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class BookingController(IPublishEndpoint publishEndpoint, ILogger<BookingController> logger) : ControllerBase
+public class BookingController(IPublishEndpoint publishEndpoint, ILogger<BookingController> logger, IApplicationDbContext context) : ControllerBase
 {
     [HttpPost]
-    public async Task<IActionResult> CreateBooking([FromBody] CreateBookingDto dto)
+    public async Task<IActionResult> CreateBooking([FromBody] CreateBookingDto dto, CancellationToken ct)
     {
         var bookingId = Guid.NewGuid();
-        
-        // In a real app, we would save to DB here first with status "New"
         
         logger.LogInformation("Creating booking {BookingId} for amount {Amount}", bookingId, dto.Amount);
 
         await publishEndpoint.Publish(new BookingCreated(bookingId, dto.ConcertId, dto.Amount));
+
+        await context.SaveChangesAsync(ct);
 
         return Accepted(new { BookingId = bookingId, Status = "Processing" });
     }
